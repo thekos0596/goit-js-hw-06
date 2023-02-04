67af97aa823eeb399ab527b2f91cc3f713c84d93
Haidai Kostiantyn <thekos0596@gmail.com>
2023-02-04 14:49:46 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,10 @@ const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", lostFocus);
 
 function lostFocus() {
+  inputEl.classList.remove("valid", "invalid");
   if (inputEl.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add("valid");
-  } else if (inputEl.value.length !== Number(inputEl.dataset.length)) {
-    inputEl.classList.remove("valid");
+  } else {
     inputEl.classList.add("invalid");
   }
 }
